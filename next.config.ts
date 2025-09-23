f71b922057156d428b64b552ab3180350f51dc4a
Rajak13 <queresma05@gmail.com>
2025-09-23 06:01:06 +0545

    Setting up Electron with Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  outputs: "export",
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
